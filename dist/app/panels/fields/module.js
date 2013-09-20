b964a94c7eddfa98e0b5a6f03a2133eff3040631
Philip Kubat <pkubat@kubat.com>
2013-09-20 08:38:06 -0400

    Third time a charm?

--- dist/app/panels/fields/module.js
+++ dist/app/panels/fields/module.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /*! kibana - v3.0.0m3pre - 2013-09-20
-=======
-/*! kibana - v3.0.0m3pre - 2013-09-18
->>>>>>> 3446c24f3a651098f0a16ece681e0509f89dd22f
  * Copyright (c) 2013 Rashid Khan; Licensed Apache License */
 
 define("panels/fields/module",["angular","app","underscore"],function(a,b,c){var d=a.module("kibana.panels.fields",[]);b.useModule(d),d.controller("fields",["$scope",function(a){a.panelMeta={status:"Deprecated",description:"You should not use this table, it does not work anymore. The table panel nowintegrates a field selector. This module will soon be removed."};var b={style:{},arrange:"vertical",micropanel_position:"right"};c.defaults(a.panel,b),a.init=function(){}}])});
